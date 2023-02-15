9b502e9def4c8b69c9f58b4df61fa1760584492d
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-02-15 16:55:37 +0530

    learned Padding

---
+++ Js/String/Temple String.js
@@ -0,0 +1,13 @@
+const name = 'Anderson';
+const Email = 'ander7@gmail.com';
+
+const msg = 'The Name Is ' + name + 'For Contact ' + Email;
+console.log(msg) // It's more confusable and stuff to concatinate instead of this we can use ->
+
+console.log(`The name is ${name} for contact ${Email}`)
+
+// $name defines the expression .
+
+// we say template but it's not template, its already a string
+
+//to know easy simple it is an example like IIFE . and IIFE works when we involed. so same thing  happens with string it is immediately goes and construct.
