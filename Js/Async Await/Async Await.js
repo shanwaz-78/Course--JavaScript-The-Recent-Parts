eb3f017016b513f0ec5fea7f5dea8299281e0268
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-02-24 19:04:55 +0530

    debug some issues

--- Js/Async Await/Async Await.js
+++ Js/Async Await/Async Await.js
@@ -1,27 +1,46 @@
-try {
-    async function fetchJsonData() {
-        console.log('This is 1st msg');
-        await fetch('https://jsonplaceholder.typicode.com/users',{
-            method : 'POST',
-            body : JSON.stringify({
-                Name : 'Kyle',
-                Age : 23,
-                Desgination : 'Developer',
-            }),
-            headers : {
-                'Content-type' : 'application/json'
-            }
-        })
-        .then((result => result.json()))
-        .then((result2 => console.log(result2)))
-        
-    }
-        console.log('This is 2nd msg');
-        fetchJsonData();
-        console.log('This is 3rd msg');
-} catch (error) {
-    console.log('Error is occured')
+async function fetchData() {
+    console.log('This is 2nd msg');
+    await fetch('https://jsonplaceholder.typicode.com/users',{
+        method : 'POST',
+        body : JSON.stringify({
+            name : 'Kyle',
+            age : 22,
+            designation : 'Developer'
+        }),
+        headers : {
+            'Content-type' : 'application/json'
+        }
+    })
+   .then((success => success.json()))
+   .then((result => console.log(result)))
 }
+console.log('This is 1st msg');
+fetchData()
+
+//  try {
+//     async function fetchJsonData() {
+//         console.log('This is 1st msg');
+//         await fetch('https://jsonplaceholder.typicode.com/users',{
+//             method : 'POST',
+//             body : JSON.stringify({
+//                 Name : 'Kyle',
+//                 Age : 23,
+//                 Desgination : 'Developer',
+//             }),
+//             headers : {
+//                 'Content-type' : 'application/json'
+//             }
+//         })
+//         .then((result => result.json()))
+//         .then((result2 => console.log(result2)))
+        
+//     }
+//         console.log('This is 2nd msg');
+//         fetchJsonData();
+//         console.log('This is 3rd msg');
+// } catch (error) {
+//     console.log('Error is occured')
+// }
 
 // async function tryFn(files) {
 //     const prs = files.map(getfile);
