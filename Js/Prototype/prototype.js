7929354d3c0f64dce4eb961a1bccdda41f315f70
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-03-08 14:23:13 +0530

    debug some issues

--- Js/Prototype/prototype.js
+++ Js/Prototype/prototype.js
@@ -17,26 +17,26 @@ obj2.__proto__ = {
 
 console.log(obj.name) // First it will find name on obj it couldn't find it it will go to prototype of obj and fnd on that it again it couldn't find them then he will find on obj2 prototype;
 
-function myForEach(callback) {
-    const newArr = []
-    for(let i = 0; i<this.length; i++){
-      newArr.push(callback(this[i]))
-    }
-    return newArr
-}
+// function myForEach(callback) {
+//     const newArr = []
+//     for(let i = 0; i<this.length; i++){
+//       newArr.push(callback(this[i]))
+//     }
+//     return newArr
+// }
 
-Array.prototype.myForEach = myForEach;
+// Array.prototype.myForEach = myForEach;
 
 // we cannot direct change the object prototype we can change it to make constructor function;
 
-function eachElement(callback) {
-    let newArr = []
-    for(let i = 0; i<this.length; i++){
-        newArr.push(callback(this[i]))
-    }
-    return newArr
-}
-Array.prototype.eachElement = eachElement;
+// function eachElement(callback) {
+//     let newArr = []
+//     for(let i = 0; i<this.length; i++){
+//         newArr.push(callback(this[i]))
+//     }
+//     return newArr
+// }
+// Array.prototype.eachElement = eachElement;
 
 // function eachElement(callback) {
 //     this.name = callback;
