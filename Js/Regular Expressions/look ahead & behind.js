f5ab26014358b43cf7cfbcfa83a32893c27e2a77
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-03-13 13:33:13 +0530

    Did practice of RegExp

--- Js/Regular Expressions/look ahead & behind.js
+++ Js/Regular Expressions/look ahead & behind.js
@@ -4,3 +4,5 @@ console.log(str.match(/(l.)$/g)); // it lookbehind ;
 console.log(str.match(/(l.)(?=o)/g));
 
 console.log(str.match(/(l.)/g))
+const regExp = /hello world/i
+console.log(str.match(regExp))
